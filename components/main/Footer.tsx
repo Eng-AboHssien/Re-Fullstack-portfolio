d925b16541f846b9cb59a8ef097238f7fd117f93
Eng-AboHssien <engabohussien@gmail.com>
2024-10-24 16:30:08 +0300

    fix-footer-responsive

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -8,8 +8,8 @@ const Footer = () => {
     <div className="w-full overflow-hidden shadow-xl shadow-[#2A0E61]/50 backdrop-blur-md px-6 py-4">
       <div className="max-w-7xl mx-auto flex flex-col items-center px-4">
         {/* Row 1: Logo and Contact Info */}
-        <div className="flex flex-row  justify-between w-full items-center mb-2">
-          <span className="flex flex-col cursor-pointer">
+        <div className="flex flex-row  justify-between w-full items-center mb-1">
+          <span className="flex flex-col cursor-pointer text-sm">
             <h1 className="bg-gradient-to-r from-[#00C0FD] to-[#E70FAA] inline-block text-transparent bg-clip-text">
               {`{007}`}
             </h1>
@@ -18,7 +18,7 @@ const Footer = () => {
             </h1>
           </span>
 
-          <div className="flex flex-col md:flex-row items-center justify-between  md:w-1/2 mt-2 md:mt-0">
+          <div className="flex flex-col md:flex-row items-center justify-between  md:w-1/2  text-sm md:mt-0">
             <p className="text-white mr-2">+001008406806</p>
             <p className="text-white mr-2">islam@gmail.com</p>
             <div className="hidden md:flex flex-row items-center space-x-3">
@@ -33,8 +33,8 @@ const Footer = () => {
         <hr className="border-t border-gray-400 w-full my-4" />
 
         {/* Row 3: Navigation Links and Attribution */}
-        <div className="flex flex-col md:flex-row justify-between items-center w-full">
-          <ul className="flex  text-white md:space-x-8 space-x-3 mb-4 md:mb-0">
+        <div className="flex flex-col md:flex-row md:text-sm text-[10px]  justify-between items-center w-full">
+          <ul className="flex  text-white md:space-x-8 space-x-2 mb-4 md:mb-0">
             <li>
               <a href="/home" className="Nav-hover">
                 Home
@@ -62,7 +62,7 @@ const Footer = () => {
             </li>
           </ul>
 
-          <span className="flex items-center md:text-lg text-sm ">
+          <span className="flex items-center">
             <h1 className="text-white mr-1">Designed and built by</h1>
             <h1 className="bg-gradient-to-r from-[#00C0FD] to-[#E70FAA] inline-block text-transparent bg-clip-text">
               Islam MG
